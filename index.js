a1097047e397327bc76ed77966545679f3455f62
sagacious-shell-7968 <tommysytc.tc@gmail.com>
2020-11-18 08:43:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution here!
+const append = ["Milo", "Otis", "Garfield"];
+const prepend = ["Milo", "Otis", "Garfield"];
+const removelast = ["Milo", "Otis", "Garfield"];
+const removefirst = ["Milo", "Otis", "Garfield"];
+append.push("Odie");
+prepend.unshift("odie");
+removelast.pop();
+removefirst.shift();
+
