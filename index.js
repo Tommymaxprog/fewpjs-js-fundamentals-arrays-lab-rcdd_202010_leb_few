7e11995dddcdfc5d66411df1ea453dee36b7f4ad
sagacious-shell-7968 <tommysytc.tc@gmail.com>
2020-11-18 08:44:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const prepend = ["Milo", "Otis", "Garfield"];
 const removelast = ["Milo", "Otis", "Garfield"];
 const removefirst = ["Milo", "Otis", "Garfield"];
 append.push("Odie");
-prepend.unshift("odie");
+append.unshift("odie");
 removelast.pop();
 removefirst.shift();
 
